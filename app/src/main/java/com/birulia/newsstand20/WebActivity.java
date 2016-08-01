daecaa5ea153bd0999b8f0aacb5b5798bac9349e
Dmitry Birulia <kinozvezda@gmail.com>
2016-07-31 21:55:00 -0700

    more small changes

--- app/src/main/java/com/birulia/newsstand20/WebActivity.java
+++ app/src/main/java/com/birulia/newsstand20/WebActivity.java
@@ -2,20 +2,29 @@ package com.birulia.newsstand20;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
-
         WebView webView = (WebView) findViewById(R.id.webview);
         String webUrl = getIntent().getStringExtra("weburl");
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webView.loadUrl(webUrl);
+
+        setupViews(webUrl);
     }
 
+    public void setupViews(String url) {
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.loadUrl(url);
+    }
 }
